cec36b6959b040fa2240e018a020f9b9eae0bdea
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-09-23 13:38:51 -0500

    Update index.js

--- src/views/react_build/index.js
+++ src/views/react_build/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Forum from './app.js';
+import Forum from './components/category.jsx';
+import './project_5.css';
 
 const forum = ReactDOM.createRoot(document.getElementById('forum'));
 forum.render(<Forum />)
-
